b39f951299c95b4b8c10265bfd279125e9dca2b2
Georgii Vlasov <honest.niceman@gmail.com>
2023-04-03 09:05:20 +0400

    Minor adjustments for the proper build

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,9 +5,7 @@ import picocli.CommandLine;
 
 public class App {
     public static void main(String[] args) {
-        String filePath1 = "app/src/main/resources/file1.json";
-        String filePath2 = "app/src/main/resources/file2.json";
-        int exitCode = new CommandLine(new DifferCommand()).execute(filePath1, filePath2);
+        int exitCode = new CommandLine(new DifferCommand()).execute(args);
         System.exit(exitCode);
     }
 }
